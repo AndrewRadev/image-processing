c4c945f630b39b8485a3c5f0b2943591f6bbb417
Andrew Radev <andrey.radev@gmail.com>
2012-05-01 09:16:49 +0300

    Invert an image

--- lib/image/contrast.rb
+++ lib/image/contrast.rb
@@ -1,5 +1,12 @@
 class Image
   module Contrast
+    def invert
+      map_pixels do |x, y, pixel|
+        intensity = Color.grayscale_intensity(pixel)
+        Color.grayscale(255 - intensity)
+      end
+    end
+
     def map_intensity(ratio)
       map_pixels do |x, y, pixel|
         intensity = Color.grayscale_intensity(pixel)
